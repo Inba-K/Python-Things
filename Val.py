e8ec990886135019fb93b9e3817bb4bf38170662
Inba-K <inba.karthikeyan18@gmail.com>
2025-04-13 19:19:22 -0700

    Input and Value switch project done

---
+++ Val.py
@@ -0,0 +1,7 @@
+a=int(input("Enter a number here: "))
+b=int(input("Enter another number here: "))
+a=str(a)
+b=float(b)
+print(a)
+print("The data type I changed the variable a to was",type(a))
+print(b)
