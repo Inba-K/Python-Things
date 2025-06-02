06039bb00a68b2c26ece1c9b4ba7c0ac365e934d
Inba-K <inba.karthikeyan18@gmail.com>
2025-06-02 13:46:23 -0700

    List project done

---
+++ List.py
@@ -0,0 +1,7 @@
+l=[1,2,3,5,6,7,8,9,10]
+j=int(input("Choose a number from 1 to 10: "))
+h=int(input("Choose a number from 1 to 10: "))
+if j==h:
+    print("You guessed the number correctly!")
+else:
+    print("Incorrect!")
