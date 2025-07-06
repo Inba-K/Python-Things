b219eca34d37c164a88091355986dbcb0d6f9d9f
Inba-K <inba.karthikeyan18@gmail.com>
2025-07-06 11:35:44 -0700

    Number of odd lines found

--- Odd.py
+++ Odd.py
@@ -1,8 +1,10 @@
-f=open('Read.txt','r')
-print(f.readline())
-f.readline()
-print(f.readline())
-f.readline()
-print(f.readline())
-f.readline()
-print(f.readline())
+odd=0
+linenum=0
+with open('Read.txt','r') as f:
+    l=f.readlines()
+    ln=len(l)
+    for linenum in range(0,ln+1):
+        linenum+=1
+        if linenum%2!=0:
+            odd+=1
+print("There are",odd,"odd lines in the file.")
