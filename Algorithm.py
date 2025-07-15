1f1357ece48de9dc0acd2389b782550386ad7ddf
Inba-K <inba.karthikeyan18@gmail.com>
2025-07-15 13:57:31 -0700

    Algorithm project done

---
+++ Algorithm.py
@@ -0,0 +1,14 @@
+def iter():
+    a=int(input("What is the first number? "))
+    b=int(input("What is the second number? "))
+    print(a*b)
+def itera():
+    n=int(input("Enter a number: "))
+    c=0
+    while n>=c:
+        a=int(input("What is the first number? "))
+        b=int(input("What is the second number? "))
+        print(a*b)
+        c+=1
+print(iter())
+print(itera())
