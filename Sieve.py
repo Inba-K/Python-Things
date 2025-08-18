96ebac1483d85624414715d235d47874dcc00262
Inba-K <inba.karthikeyan18@gmail.com>
2025-08-17 18:39:54 -0700

    Sieve project done

---
+++ Sieve.py
@@ -0,0 +1,14 @@
+a=int(input("Enter a number: "))
+
+b=[True]*(a+1)
+c=2
+while c*c<=a:
+    if b[c]:
+        for i in range(c*c,a+1,c):
+            b[i]=False
+    c+=1
+d=[]
+for c in range(2,a+1):
+    if b[c]:
+        d.append(c)
+print(d)
